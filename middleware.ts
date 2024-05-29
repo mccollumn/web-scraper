3698dc947b7ba3f5c8767aa8c8d05ba127707f93
Nick McCollum <mccollumn@webtrends.com>
2024-05-28 18:03:01 -0700

    Next Auth

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,1 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-const loginMap: any = {};
-
-export function middleware(request: NextRequest) {
-  const ip: any = request.headers.get("x-forwarded-for");
-
-  if (!loginMap[ip]) {
-    loginMap[ip] = true;
-    return NextResponse.rewrite(new URL("/login", request.url));
-  }
-
-  if (request.nextUrl.pathname.startsWith("/logout")) {
-    loginMap[ip] = false;
-    return NextResponse.rewrite(new URL("/login", request.url));
-  }
-}
+export { default } from "next-auth/middleware";
